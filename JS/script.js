f307cb544f49d2671aebafe3fe3669096020657a
Nuriddagal <creakgam@gmail.com>
2022-02-15 15:25:15 +0300

    SWiper-true

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,33 @@
-import Swiper from 'swiper/bundle';
-const slider = document.querySelector('.swiper');
+const slider = document.querySelector(".swiper");
 
 function mobileSlider() {
-  if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
-   const swiper = new Swiper(slider, {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      loop: true,
-      slideClass: 'swiper-slide',
+  if (window.innerWidth <= 768 && slider.dataset.mobile === "false") {
+    const swiper = new Swiper(slider, {
+      slidesPerView: 1.5,
+      spaceBetween: 0,
+      slideClass: "swiper-slide",
       pagination: {
-       	el: '.swiper-pagination',
-          clickable: true,
-            },
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev'
-            }
-           
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     });
 
-    slider.dataset.mobile = 'true';
+    slider.dataset.mobile = "true";
   }
 
   if (window.innerWidth > 768) {
-    slider.dataset.mobile = 'false';
-    if (slider.classList.contains('swiper-container-initialized')) {
+    slider.dataset.mobile = "false";
+    if (slider.classList.contains("swiper-container-initialized")) {
       mySwiper.destroy();
     }
   }
-  console.log('slider')
-};
+  console.log(mobileSlider);
+}
 
-console.log(mobileSlider());
-
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   mobileSlider();
-})
+});
