22659c34277b0cc112e6e1394b67a05b3ed9d0c7
Nuriddagal <creakgam@gmail.com>
2022-02-15 17:57:38 +0300

    fix again

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,27 @@
 const slider = document.querySelector(".swiper");
 
 function mobileSlider() {
-  if (window.innerWidth <= 438 && slider.dataset.mobile === "false") {
+  if (window.innerWidth <= 428 && slider.dataset.mobile === "false") {
     const swiper = new Swiper(slider, {
-      slidesPerView: 1,
-      spaceBetween: 32,
+      slidesPerView: 1.2,
+      spaceBetween: 12,
       slideClass: "swiper-slide",
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
     });
 
-    slider.dataset.mobile = "false";
+    slider.dataset.mobile = "true";
   }
 
   if (window.innerWidth > 768) {
     slider.dataset.mobile = "false";
-    slider.classList.remove("swiper");
     if (slider.classList.contains("swiper-container-initialized")) {
       swiper.destroy();
     }
-  } else {
-    slider.classList.add("swiper");
   }
 }
-
 window.addEventListener("resize", () => {
   mobileSlider();
 });
